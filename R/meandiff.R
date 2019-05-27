cfb2d75df9ffc50c8bab5d77147bf4080e555c73
Choi Jin Young <linkinjy@nate.com>
2019-05-27 19:40:12 +0900

    meandiff

--- R/meandiff.R
+++ R/meandiff.R
@@ -2,7 +2,7 @@
 #'
 #' population mean difference
 #'
-#' @param formula,method
+#' @param formula,method,fac,two,ran
 #'
 #' @return diff,lwr.ci,upr,ci,p-value,ci plot
 #'
@@ -22,31 +22,216 @@
 #' @importFrom
 #' DescTools PostHocTest
 
-mean.diff<-function(formula,method="lsd"){
+mean.diff<-function(formula,method="lsd",fac=NULL,two=FALSE,ran=FALSE){
   s<-aov(formula)
-  if(method=="hsd"){
-    as<-PostHocTest(s,method="hsd")
-    print(as)
-    plot(as)
-  }else if(method=="bonferroni"){
-    as<-PostHocTest(s,method="bonferroni")
-    print(as)
-    plot(as)
-  }else if(method=="scheffe"){
-    as<-PostHocTest(s,method="scheffe")
-    print(as)
-    plot(as)
-  }else if(method=="duncan"){
-    as<-PostHocTest(s,method="duncan")
-    print(as)
-    plot(as)
-  }else if(method=="newmankeuls"){
-    as<-PostHocTest(s,method="newmankeuls")
-    print(as)
-    plot(as)
+
+  if(ran==FALSE){
+    if(method=="hsd"){
+      as<-PostHocTest(s,method="hsd")
+      par(mfrow=c(2,2))
+      print(as)
+      plot(as)
+    }else if(method=="bonferroni"){
+      as<-PostHocTest(s,method="bonferroni")
+      par(mfrow=c(2,2))
+      print(as)
+      plot(as)
+    }else if(method=="scheffe"){
+      as<-PostHocTest(s,method="scheffe")
+      par(mfrow=c(2,2))
+      print(as)
+      plot(as)
+    }else if(method=="duncan"){
+      as<-PostHocTest(s,method="duncan")
+      par(mfrow=c(2,2))
+      print(as)
+      plot(as)
+    }else if(method=="newmankeuls"){
+      as<-PostHocTest(s,method="newmankeuls")
+      par(mfrow=c(2,2))
+      print(as)
+      plot(as)
+    }else{
+      as<-PostHocTest(s,method="lsd")
+      par(mfrow=c(2,2))
+      print(as)
+      plot(as)
+    }
   }else{
-    as<-PostHocTest(s,method="lsd")
-    print(as)
-    plot(as)
+    if(method=="hsd"){
+      if(fac=="a"&&two==FALSE){
+        as<-PostHocTest(s,which="a",method="hsd")
+        print(as)
+        plot(as)
+      }else if(fac=="b"&&two==FALSE){
+        as<-PostHocTest(s,which="b",method="hsd")
+        print(as)
+        plot(as)
+      }else if(fac=="c"&&two==FALSE){
+        as<-PostHocTest(s,which="c",method="hsd")
+        print(as)
+        plot(as)
+      }else if(fac=="a"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="hsd")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="b"&&two==TRUE){
+        as<-PostHocTest(s,which=c("a","c"),method="hsd")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="c"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="hsd")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }
+    }else if(method=="bonferroni"){
+      if(fac=="a"&&two==FALSE){
+        as<-PostHocTest(s,which="a",method="bonferroni")
+        print(as)
+        plot(as)
+      }else if(fac=="b"&&two==FALSE){
+        as<-PostHocTest(s,which="b",method="bonferroni")
+        print(as)
+        plot(as)
+      }else if(fac=="c"&&two==FALSE){
+        as<-PostHocTest(s,which="c",method="bonferroni")
+        print(as)
+        plot(as)
+      }else if(fac=="a"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="bonferroni")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="b"&&two==TRUE){
+        as<-PostHocTest(s,which=c("a","c"),method="bonferroni")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="c"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="bonferroni")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }
+    }else if(method=="scheffe"){
+      if(fac=="a"&&two==FALSE){
+        as<-PostHocTest(s,which="a",method="scheffe")
+        print(as)
+        plot(as)
+      }else if(fac=="b"&&two==FALSE){
+        as<-PostHocTest(s,which="b",method="scheffe")
+        print(as)
+        plot(as)
+      }else if(fac=="c"&&two==FALSE){
+        as<-PostHocTest(s,which="c",method="scheffe")
+        print(as)
+        plot(as)
+      }else if(fac=="a"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="scheffe")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="b"&&two==TRUE){
+        as<-PostHocTest(s,which=c("a","c"),method="scheffe")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="c"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="scheffe")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }
+    }else if(method=="duncan"){
+      if(fac=="a"&&two==FALSE){
+        as<-PostHocTest(s,which="a",method="duncan")
+        print(as)
+        plot(as)
+      }else if(fac=="b"&&two==FALSE){
+        as<-PostHocTest(s,which="b",method="duncan")
+        print(as)
+        plot(as)
+      }else if(fac=="c"&&two==FALSE){
+        as<-PostHocTest(s,which="c",method="duncan")
+        print(as)
+        plot(as)
+      }else if(fac=="a"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="duncan")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="b"&&two==TRUE){
+        as<-PostHocTest(s,which=c("a","c"),method="duncan")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="c"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="duncan")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }
+    }else if(method=="newmankeuls"){
+      if(fac=="a"&&two==FALSE){
+        as<-PostHocTest(s,which="a",method="newmankeuls")
+        print(as)
+        plot(as)
+      }else if(fac=="b"&&two==FALSE){
+        as<-PostHocTest(s,which="b",method="newmankeuls")
+        print(as)
+        plot(as)
+      }else if(fac=="c"&&two==FALSE){
+        as<-PostHocTest(s,which="c",method="newmankeuls")
+        print(as)
+        plot(as)
+      }else if(fac=="a"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="newmankeuls")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="b"&&two==TRUE){
+        as<-PostHocTest(s,which=c("a","c"),method="newmankeuls")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="c"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="newmankeuls")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }
+    }else{
+      if(fac=="a"&&two==FALSE){
+        as<-PostHocTest(s,which="a",method="lsd")
+        print(as)
+        plot(as)
+      }else if(fac=="b"&&two==FALSE){
+        as<-PostHocTest(s,which="b",method="lsd")
+        print(as)
+        plot(as)
+      }else if(fac=="c"&&two==FALSE){
+        as<-PostHocTest(s,which="c",method="lsd")
+        print(as)
+        plot(as)
+      }else if(fac=="a"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="lsd")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="b"&&two==TRUE){
+        as<-PostHocTest(s,which=c("a","c"),method="lsd")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }else if(fac=="c"&&two==TRUE){
+        as<-PostHocTest(s,which=c("b","c"),method="lsd")
+        print(as)
+        par(mfrow=c(1,2))
+        plot(as)
+      }
+    }
   }
 }
